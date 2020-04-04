4a020a89540354dd97666138af2f95f7b8e6a980
Alex Thiessen <alex.thiessen.de+github@gmail.com>
2020-04-04 14:52:27 +0200

    script/install: Reformat for readability
    Add line breaks and split the long installation statement.

--- script/install-bats.sh
+++ script/install-bats.sh
@@ -2,5 +2,12 @@
 set -o errexit
 set -o xtrace
 
-git clone --depth 1 https://github.com/sstephenson/bats
-cd bats && ./install.sh "${HOME}/.local" && cd .. && rm -rf bats
+git clone \
+  --depth 1 \
+  https://github.com/sstephenson/bats
+
+(cd bats
+  ./install.sh "${HOME}/.local"
+)
+
+rm -rf bats
